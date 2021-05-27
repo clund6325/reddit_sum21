2364fdec1997ea04fc207a3f179ceca7c53000ed
Cameron Lund <clund6314@gmail.com>
2021-05-26 18:57:52 -0600

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,21 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # Route - allows us to communicate through the browser into the application
+  # rails commands, http verb, url, endpoint
+  # resources - generate all necessary routes for main actions
+    # resources :controller
+  # root - onces , this will define the root page, starting page
+    # root 'controller#action'
+  # long way, defining own routes
+    # http verb, url, endpoint, rails command
+   # get 'people/new', to: 'people#index'
+   # post
+  root 'subs#index'
+
+  resources :subs do
+    resources :topics
+  end
+
+  resources :topics do
+    resources :comments
+  end
 end
